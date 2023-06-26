ab90abd16e6866ee4835064187cdd8f61e17278c
Amine <amineleboss10@gmail.com>
2023-06-26 12:38:19 +0100

    github change

--- vagrant/Automated_provisioning/tomcat.sh
+++ vagrant/Automated_provisioning/tomcat.sh
@@ -46,7 +46,7 @@ systemctl daemon-reload
 systemctl start tomcat
 systemctl enable tomcat
 
-git clone -b local-setup https://github.com/AmineMadani/ysj-app-local
+git clone https://github.com/AmineMadani/ysj-app-local
 cd vprofile-project
 mvn install
 systemctl stop tomcat
